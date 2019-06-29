a5a742b1c178e2cb2e5bb39dc914196ec3e985b1
squteronsquter <squter@squter.pl>
2019-06-29 15:33:21 +0200

    Selected article is being displayed after it has been created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
+
   def new
     @article = Article.new
   end
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -11,6 +13,11 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :description)
